596f91df09d962a14a9fcb713bcef95cde9f89c5
Amirreza Mohammadi <m.senator.6247@gmail.com>
2021-07-21 20:38:05 +0430

    Add project files.

---
+++ SakhtemanProj/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sakhteman
+{
+    class Program
+    { 
+        //12.5 + ( sum - E )  / A ^ ~ 5 ^ ~ 0.17
+        static void Main(string[] args)
+        {
+            Console.Write("Equation : ");
+            String eq = Console.ReadLine();
+
+            Console.WriteLine("--------------------------------------------------------------");
+            String postfix = PostfixStack(eq).ConvertToString();
+            Console.WriteLine($"Postfix => {postfix}");
+
+            Console.WriteLine("--------------------------------------------------------------");
+            String infix = Infix(eq).ConvertToString();
+            Console.WriteLine($"Infix => {infix}");
+
+            Console.ReadKey();
+        }
+
+        static Stack<char> PostfixStack(String text)
+        {
+            Stack<char> stack = new Stack<char>();
+            Stack<char> symblesStack = new Stack<char>();
+            Stack<char> braceStack = new Stack<char>();
+            bool endBrace = true;
+            char neg = ' ';
+
+            foreach (char word in text)
+            {
+                if (word == '~')
+                {
+                    neg = '~';
+                }
+                else if (!word.IsBraces() && endBrace)
+                {
+                    if (neg == '~' && stack.Peek() >= '1' && stack.Peek() <= '9' && word == ' ')
+                    {
+                        stack.Push('~');
+                        neg = ' ';
+                    }
+
+                    if (word.IsSymble())
+                    {
+                        symblesStack.Push(word);
+                    }
+                    else if (!word.IsBraces())
+                    {
+                        stack.Push(word);
+                    }
+                }
+                else if (word == ')')
+                {
+                    stack.Merge(braceStack);
+                    braceStack.Clear();
+                    endBrace = true;
+                }
+                else
+                {
+                    if (neg == '~' && stack.Peek() >= '1' && stack.Peek() <= '9' && word == ' ')
+                    {
+                        stack.Push('~');
+                        neg = ' ';
+                    }
+
+                    if (word.IsSymble())
+                    {
+                        braceStack.Push(word);
+                    }
+                    else if (!word.IsBraces())
+                    {
+                        stack.Push(word);
+                    }
+                    endBrace = false;
+                }
+            }
+
+            if (neg != ' ')
+                stack.Push(neg);
+
+            stack.Push(' ');
+            stack.Merge(symblesStack);
+            return stack.RemoveWhiteSpace();
+        }
+
+        static Stack<char> Infix(String text)
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push('(');
+            foreach (char ch in text)
+            {
+                if (ch == ' ' && stack.Peek() != '(' && stack.Peek() != '~')
+                {
+                    stack.Push('(');
+                }
+                else if (ch == '~')
+                {
+                    stack.Push('(');
+                    stack.Push('~');
+                }
+                else if (ch == '^')
+                {
+                    stack.Pop();
+                    if (stack.Peek() != 'A')
+                        stack.Push(')');
+                    stack.Push('^');
+                }
+                else if (ch != ' ')
+                {
+                    if (ch.IsSymble() || (ch == ')' && stack.Peek() == '('))
+                    {
+                        stack.Pop();
+                    }
+                    if (ch.IsWord() && (stack.Peek() == '(' || stack.Peek() == ' ') && ch != 'A')
+                    {
+                        stack.Pop();
+                    }
+                    stack.Push(ch);
+                }
+            }
+            stack = stack.AddEndBrace(stack);
+            return stack.Reverse();
+        }
+
+
+    }
+}
---
+++ SakhtemanProj/Utility.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakhteman
+{
+    public static class Utility
+    {
+        public static bool IsSymble(this char ch)
+        {
+            return ch == '-' || ch == '+' || ch == '*' || ch == '/' || ch == '^' || ch == '~';
+        }
+
+        public static bool IsBraces(this char ch)
+        {
+            return ch == ')' || ch == '(';
+        }
+
+        public static bool IsWord(this char ch)
+        {
+            return ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z';
+        }
+
+        public static Stack<char> Merge(this Stack<char> stack, Stack<char> mergeStack)
+        {
+            while (mergeStack.Count != 0)
+            {
+                stack.Push(mergeStack.Pop());
+            }
+            return stack;
+        }
+
+        public static Stack<char> Reverse(this Stack<char> stack)
+        {
+            Stack<char> reverseStack = new Stack<char>();
+            while (stack.Count != 0)
+            {
+                reverseStack.Push(stack.Pop());
+            }
+            return reverseStack;
+        }
+
+        public static Stack<char> RemoveWhiteSpace(this Stack<char> stack)
+        {
+            Stack<char> nonSpaceStack = new Stack<char>();
+            while (stack.Count > 0)
+            {
+                if (stack.Peek() != ' ')
+                {
+                    nonSpaceStack.Push(stack.Pop());
+                }
+                else
+                {
+                    stack.Pop();
+                    if (stack.Peek() != ' ')
+                    {
+                        nonSpaceStack.Push(' ');
+                    }
+                }
+            }
+            return nonSpaceStack;
+        }
+
+        public static String ConvertToString(this Stack<char> stack)
+        {
+            StringBuilder text = new StringBuilder();
+            while (stack.Count != 0)
+                text.Append(stack.Pop());
+
+            return text.ToString();
+        }
+
+
+        public static Stack<char> AddEndBrace(this Stack<char> stack, Stack<char> s)
+        {
+            int beginBrace = stack.Where(u => u.Equals('(')).Count();
+            int endBrace = stack.Where(u => u.Equals(')')).Count();
+
+            for (int i = 0; i < beginBrace - endBrace; i++)
+                s.Push(')');
+            return s;
+        }
+    }
+
+}
